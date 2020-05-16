ad6a242fe1e687e51f847247a1749bf8f5aad567
statist32 <statist32@hotmail.de>
2020-05-16 14:49:50 +0200

    added semicolons and double quotes

--- public/client.js
+++ public/client.js
@@ -1,66 +1,64 @@
-const gameStateDisplay = document.getElementById('game-state-display')
-const canvas = document.getElementById('canvas')
-const ctx = canvas.getContext('2d')
-const TILE_SIZE = 20
-const ROWS = 21
-const COLUMNS = 21
+const gameStateDisplay = document.getElementById("game-state-display");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const TILE_SIZE = 20;
+const ROWS = 21;
+const COLUMNS = 21;
 
-canvas.height = ROWS * TILE_SIZE
-canvas.width = COLUMNS * TILE_SIZE
+canvas.height = ROWS * TILE_SIZE;
+canvas.width = COLUMNS * TILE_SIZE;
 
-const C_HEIGHT = canvas.height
-const C_WIDTH = canvas.width
+const C_HEIGHT = canvas.height;
+const C_WIDTH = canvas.width;
 
-const socket = io()
+const socket = io();
 
-let board
-let player
+let board;
+let player;
 
-const ROOM = window.location.hash
-console.log(ROOM)
-socket.on('connect', () => {
-  socket.emit('join', ROOM)
-})
+const ROOM = window.location.hash;
 
-socket.on('test', (msg) => {
-  console.log(msg)
-})
+socket.on("connect", () => {
+  socket.emit("join", ROOM);
+});
 
-socket.on('update', (newBoard) => {
-  board = JSON.parse(newBoard)
-  // console.log('update')
-  // console.table(newBoard)
-})
-socket.on('gameStateUpdate', (state) => (gameStateDisplay.innerText = state))
+socket.on("test", (msg) => {
+  console.log(msg);
+});
+
+socket.on("update", (newBoard) => {
+  board = JSON.parse(newBoard);
+});
+socket.on("gameStateUpdate", (state) => (gameStateDisplay.innerText = state));
 
 function handleInput(e) {
   switch (e.key) {
-    case 'w':
-      socket.emit('move', 'moveUp')
-      break
+    case "w":
+      socket.emit("move", "moveUp");
+      break;
 
-    case 'a':
-      socket.emit('move', 'moveLeft')
-      break
+    case "a":
+      socket.emit("move", "moveLeft");
+      break;
 
-    case 's':
-      socket.emit('move', 'moveDown')
-      break
+    case "s":
+      socket.emit("move", "moveDown");
+      break;
 
-    case 'd':
-      socket.emit('move', 'moveRight')
-      break
-    case ' ':
-      socket.emit('plantBomb')
-      break
+    case "d":
+      socket.emit("move", "moveRight");
+      break;
+    case " ":
+      socket.emit("plantBomb");
+      break;
     default:
-      break
+      break;
   }
 }
-document.addEventListener('keypress', handleInput)
+document.addEventListener("keypress", handleInput);
 
 function animate() {
-  ctx.clearRect(0, 0, C_WIDTH, C_HEIGHT)
+  ctx.clearRect(0, 0, C_WIDTH, C_HEIGHT);
 
   board &&
     board.forEach((row, rowIndex) => {
@@ -68,56 +66,56 @@ function animate() {
         if (tile !== 0) {
           switch (tile) {
             case 1:
-              drawPlayer(ctx, rowIndex, tileIndex)
-              break
+              drawPlayer(ctx, rowIndex, tileIndex);
+              break;
             case 2:
-              drawBlock(ctx, rowIndex, tileIndex, true)
-              break
+              drawBlock(ctx, rowIndex, tileIndex, true);
+              break;
             case 3:
-              drawBlock(ctx, rowIndex, tileIndex, false)
-              break
+              drawBlock(ctx, rowIndex, tileIndex, false);
+              break;
             case 4:
-              drawBomb(ctx, rowIndex, tileIndex)
-              break
+              drawBomb(ctx, rowIndex, tileIndex);
+              break;
             case 5:
-              drawFire(ctx, rowIndex, tileIndex)
-              break
+              drawFire(ctx, rowIndex, tileIndex);
+              break;
             default:
-              break
+              break;
           }
         }
-      })
-    })
-  requestAnimationFrame(animate)
+      });
+    });
+  requestAnimationFrame(animate);
 }
 
-animate()
+animate();
 
 function drawPlayer(ctx, row, column) {
-  ctx.fillStyle = 'green'
-  ctx.fillRect(column * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE)
+  ctx.fillStyle = "green";
+  ctx.fillRect(column * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE);
 }
 
 function drawBlock(ctx, row, column, breakable = false) {
-  ctx.fillStyle = breakable ? 'brown' : 'grey'
-  ctx.fillRect(column * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE)
+  ctx.fillStyle = breakable ? "brown" : "grey";
+  ctx.fillRect(column * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE);
 }
 
 function drawBomb(ctx, row, column) {
-  ctx.fillStyle = 'black'
-  ctx.beginPath()
-  const radius = TILE_SIZE / 2
+  ctx.fillStyle = "black";
+  ctx.beginPath();
+  const radius = TILE_SIZE / 2;
   ctx.arc(
     column * TILE_SIZE + TILE_SIZE / 2,
     row * TILE_SIZE + TILE_SIZE / 2,
     radius,
     0,
     2 * Math.PI
-  )
-  ctx.fill()
+  );
+  ctx.fill();
 }
 
 function drawFire(ctx, row, column) {
-  ctx.fillStyle = 'red'
-  ctx.fillRect(column * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE)
+  ctx.fillStyle = "red";
+  ctx.fillRect(column * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE);
 }
